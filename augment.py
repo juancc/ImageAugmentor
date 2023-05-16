b7048c89b7ffd624a0cf4901dfb8cbccd370f69a
Juanc <jkaz.os@gmail.com>
2023-05-16 11:01:10 -0500

    Verbose, identifier params.

--- augment.py
+++ augment.py
@@ -36,11 +36,15 @@ parser = argparse.ArgumentParser(
 parser.add_argument('dataset', help='Path to dataset')
 parser.add_argument('-c', '--childs', help='Number of random variations per source file', default=3)
 parser.add_argument('-s', '--size', help='Fix square output image size', default=0)
-parser.add_argument('-q', '--quality', help='Output file image quality', default=90) 
+parser.add_argument('-q', '--quality', help='Output file image quality', default=90)
+parser.add_argument('-v', '--verbose', help='Show errors', action='store_true')
+parser.add_argument('-i', '--identifier', help='Name added to saved images', default='aug')
 
 
 
-def augment(path, childs, target_size, quality):
+
+
+def augment(path, childs, target_size, quality, verbose, ident):
     """
     : path (Str) : path to dataset
     """
@@ -71,7 +75,7 @@ def augment(path, childs, target_size, quality):
                 bck = cv2.imread(bck_filepath)
                 aug = background(src, bck, add_noise=True, flip=True)*255
 
-                out_filename = f'{filepath.name.split(".")[0]} aug-{c}.jpg'
+                out_filename = f'{filepath.name.split(".")[0]} {ident}-{c}.jpg'
                 out_filepath = os.path.join(output_dir, out_filename)
                 target_size = int(target_size)
                 if target_size>0:
@@ -80,6 +84,7 @@ def augment(path, childs, target_size, quality):
                 cv2.imwrite(out_filepath, aug, [int(cv2.IMWRITE_JPEG_QUALITY), int(quality)])
 
         except Exception as e:
+            if verbose: print(f'Err:{e} in file: {filepath}')
             err.append(str(filepath))
 
 
@@ -91,4 +96,4 @@ def augment(path, childs, target_size, quality):
 if __name__ == '__main__':
     args = parser.parse_args()
 
-    augment(args.dataset, args.childs, args.size, args.quality)
+    augment(args.dataset, args.childs, args.size, args.quality, args.verbose, args.identifier)
